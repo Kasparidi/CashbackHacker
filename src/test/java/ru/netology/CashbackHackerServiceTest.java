54fe13c1108f84d9488cf995eb85251fd2aec4ba
Kasparidi <kasparidi@gmail.com>
2020-11-17 15:47:18 +0300

    fix tests

--- src/test/java/ru/netology/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/CashbackHackerServiceTest.java
@@ -1,6 +1,5 @@
 package ru.netology;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,16 +11,15 @@ public class CashbackHackerServiceTest {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 900;
         int expected = 100;
-        assertEquals(service.remain(amount), expected);
+        assertEquals(expected, service.remain(amount));
     }
 
-    @Ignore
     @Test
     public void checkBoundary() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 1000;
         int expected = 0;
-        assertEquals(service.remain(amount), expected);
+        assertEquals(expected, service.remain(amount));
     }
 
     @Test
@@ -29,15 +27,14 @@ public class CashbackHackerServiceTest {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 1800;
         int expected = 200;
-        assertEquals(service.remain(amount), expected);
+        assertEquals(expected, service.remain(amount));
     }
 
-    @Ignore
     @Test
     public void checkDoubleBoundary() {
         CashbackHackerService service = new CashbackHackerService();
         int amount = 2000;
         int expected = 0;
-        assertEquals(service.remain(amount), expected);
+        assertEquals(expected, service.remain(amount));
     }
 }
